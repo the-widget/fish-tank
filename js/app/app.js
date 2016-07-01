fbdb7c42eb6b615ece19f989526fc205bf8f9d2b
the-widget <brennenawana108ny@gmail.com>
2016-07-01 08:26:27 -0400

    index.html displays list of posts from MainCtrl

--- js/app/app.js
+++ js/app/app.js
@@ -3,4 +3,15 @@ angular
   .controller('MainCtrl', ['$scope', 
     function($scope){
       $scope.test = 'Hello world!';
-    }]);
+
+      $scope.posts = [
+      {title: 'post 1'},
+      {title: 'post 2'},
+      {title: 'post 3'},
+      {title: 'post 4'},
+      {title: 'post 5'}
+      ];
+
+
+    }
+  ]);
