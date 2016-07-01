771a894fba087fa35b1e13fc44eed6a682f72ea6
the-widget <brennenawana108ny@gmail.com>
2016-07-01 12:49:39 -0400

    created comments link to render post state - only displays title

--- js/app/app.js
+++ js/app/app.js
@@ -35,7 +35,10 @@ angular
 
   //--> FILLER POSTS
        $scope.posts.push(
-      {title: 'post 1', upvotes: 5},
+      {title: 'post 1', upvotes: 5, comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]},
       {title: 'post 2', upvotes: 2},
       {title: 'post 3', upvotes: 15},
       {title: 'post 4', upvotes: 9},
@@ -65,5 +68,5 @@ angular
     '$stateParams',
     'postsFactory',
     function($scope, $stateParams, postsFactory){
-
+      $scope.post = postsFactory.posts[$stateParams.id];
     }]);
