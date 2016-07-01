16d582040a89bb03a01398921f8839a1d627727e
the-widget <brennenawana108ny@gmail.com>
2016-07-01 11:35:46 -0400

    created posts factory-service

--- js/app/app.js
+++ js/app/app.js
@@ -1,16 +1,30 @@
 angular
   .module('fishTank', [])
-  .controller('MainCtrl', ['$scope', 
-    function($scope){
+  .factory('posts', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+
+  }])
+
+  .controller('MainCtrl', [
+    '$scope', 
+    'posts', 
+    function($scope, posts){
       $scope.test = 'Hello world!';
 
-      $scope.posts = [
+       $scope.posts = posts.posts;
+       
+  //--> FILLER POSTS
+       $scope.posts.push(
       {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
       {title: 'post 3', upvotes: 15},
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
-      ];
+      );
+  //<--
 
       $scope.addPost = function(){
         if(!$scope.title || $scope.title === '') {return;}
