4618a7983783e02814cd6344df98c8ce210e59ab
the-widget <brennenawana108ny@gmail.com>
2016-07-01 08:29:00 -0400

    posts have upvotes and are sorted in descending order

--- js/app/app.js
+++ js/app/app.js
@@ -5,13 +5,13 @@ angular
       $scope.test = 'Hello world!';
 
       $scope.posts = [
-      {title: 'post 1'},
-      {title: 'post 2'},
-      {title: 'post 3'},
-      {title: 'post 4'},
-      {title: 'post 5'}
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
       ];
 
-
+      
     }
   ]);
