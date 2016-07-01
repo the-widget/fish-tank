b5b73a504e502e706b57933b731f3794b86b6d49
the-widget <brennenawana108ny@gmail.com>
2016-07-01 08:35:23 -0400

    user can create a new post with title

--- js/app/app.js
+++ js/app/app.js
@@ -12,6 +12,12 @@ angular
       {title: 'post 5', upvotes: 4}
       ];
 
+      $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '') {return;}
+        $scope.posts.push({title: $scope.title, upvotes: 0});
+        $scope.title = '';
+      }
+
       $scope.incrementUpvotes = function(post) {
         post.upvotes += 1;
       }
