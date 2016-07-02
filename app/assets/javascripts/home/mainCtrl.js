26d9c0cc77c5c02fa467567b2964b19dffcbd400
the-widget <brennenawana108ny@gmail.com>
2016-07-02 11:19:36 -0400

    generated models for post and comment declared associations

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -4,19 +4,6 @@
     function($scope, postsFactory){
       $scope.posts = postsFactory.posts;
 
-  //--> FILLER POSTS
-      $scope.posts.push(
-        {title: 'post 1', upvotes: 5, comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4}
-      );
-  //<--
-
       $scope.addPost = function(){
         if(!$scope.title || $scope.title === '') {return;}
         $scope.posts.push({
