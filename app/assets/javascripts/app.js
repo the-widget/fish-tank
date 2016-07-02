96e9c7693381b28f6d3eecc0de7a097bacdb07a2
the-widget <brennenawana108ny@gmail.com>
2016-07-02 16:24:54 -0400

    added module states for login and registration

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -24,6 +24,16 @@ angular
               return postsFactory.get($stateParams.id);
             }]
           }
+        })
+        .state('login', {
+          url: '/login',
+          templateUrl: 'auth/_login.html',
+          controller: 'AuthCtrl'
+        })
+        .state('register', {
+          url: '/register',
+          templateUrl: 'auth/_register.html',
+          controller: 'AuthCtrl'
         });
 
       $urlRouterProvider.otherwise('home');
