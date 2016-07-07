4dfdf4068d6c9d04b4a7bfd1b39c56f5a1a3c01b
the-widget <brennenawana108ny@gmail.com>
2016-07-06 23:36:52 -0400

    added login/out messages on timeout

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -11,14 +11,22 @@ angular.module('fishTank')
       });
 
       $scope.$on('devise:new-registration', function(event, user){
+        $('p.alert').html("Congratulations, You signed up!")
+        setTimeout(function(){ $('p.alert').html(""); }, 3000);
+
         $scope.user = user;
       });
 
       $scope.$on('devise:login', function(event, user){
+        $('p.alert').html("Logged in as " + user.username)
+        setTimeout(function(){ $('p.alert').html(""); }, 3000);
         $scope.user = user;
+
       });
 
       $scope.$on('devise:logout', function(event, user){
+        $('p.alert').html("You successfully signed out!")
+        setTimeout(function(){ $('p.alert').html(""); }, 3000);
         $scope.user = {};
       });
     }]);
